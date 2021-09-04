0a1923323fb46439f4b050f9c129dc58877586d6
Carlos Calla <calla.alarcon@gmail.com>
2021-09-04 04:56:38 -0500

    last update

--- nvim/lua/carlos/telescope.lua
+++ nvim/lua/carlos/telescope.lua
@@ -1,16 +1,15 @@
-require'nvim-web-devicons'.setup {
- -- your personnal icons can go here (to override)
- -- DevIcon will be appended to `name`
- override = {
-  zsh = {
-    icon = "",
-    color = "#428850",
-    name = "Zsh"
+require('telescope').setup {
+  extensions = {
+    fzf = {
+      override_generic_sorter = false, -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
   }
- };
- -- globally enable default icons (default to false)
- -- will get overriden by `get_icons` option
- default = true;
 }
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
 
-require'nvim-web-devicons'.get_icons()
+require('telescope').load_extension('coc')
