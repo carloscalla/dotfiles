295504b8cedc3b8c564be64301e837caf1a46c50
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2022-09-02 22:24:16 +0200

    harpoon in telescope

--- dot_config/nvim/after/plugin/telescope.rc.lua
+++ dot_config/nvim/after/plugin/telescope.rc.lua
@@ -1,6 +1,7 @@
 require('telescope').setup {
   extensions = {
     fzf = {
+      fuzzy = true,
       override_generic_sorter = false, -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
@@ -12,9 +13,9 @@ require('telescope').setup {
 -- load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
 
--- require('telescope').load_extension('coc')
 
 require("telescope").load_extension "file_browser"
+
 
 -- vim.api.nvim_set_keymap(
 --   "n",
