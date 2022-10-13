8a5318158cb3bb665369c84eb383750fbe9e5183
Carlos Calla Alarcón <calla.alarcon@gmail.com>
2022-10-13 15:08:48 +0200

    fix memory leak bug

--- dot_config/nvim/plugin/null-ls.rc.lua
+++ dot_config/nvim/plugin/null-ls.rc.lua
@@ -2,8 +2,8 @@ local null_ls = require("null-ls")
 
 local sources = {
   null_ls.builtins.formatting.prettier,
-  null_ls.builtins.diagnostics.eslint,
-  null_ls.builtins.code_actions.eslint,
+  -- null_ls.builtins.diagnostics.eslint_d,
+  -- null_ls.builtins.code_actions.eslint_d,
   null_ls.builtins.formatting.clang_format,
 }
 
